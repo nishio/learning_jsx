2082c2dc51f77d84aec58919321e9d5e3617e4c8
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2012-06-01 17:28:42 +0900

    refactor

--- use_native_class/jquery-1.7.jsx
+++ use_native_class/jquery-1.7.jsx
@@ -1,36 +1,37 @@
-/** jQuery */
+/** jQuery wrapper for JSX
+by NISHIO Hirokazu
+*/
+
 import "js.jsx";
 //import "js/web.jsx"; // too slow
-native class Element {
+native class Element { // come from js/web.jsx
     var innerHTML : string;
 }
 
-final class jQuery { // entrypoint to jQuery
-    static function as_func(arg : string) : jQueryResult {
-        var jq = js.global["jQuery"] as function(:string):jQueryResult;
-        var result = jq(arg);
-	return new jQueryResult(result);
-    }
-}
+final class jQuery { // wrapper of jQuery object
+    var _t : variant; // true jQuery object
 
-class jQueryResult {
-    var _v : variant;
-    function constructor(v: variant) {
-        this._v = v;
+    function constructor(arg1 : variant, arg2 : variant){
+        var jq = js.global["jQuery"] as function(:variant, :variant):jQuery;
+        this._t = jq(arg1, arg2);
     }
 
-    function get(arg : variant) : Element {
-        var t = this._v as __noconvert__ Map.<Element>;
-        return t[arg as string];
+    function constructor(arg1 : variant){
+        var jq = js.global["jQuery"] as function(:variant):jQuery;
+        this._t = jq(arg1);
     }
 
-    function css(a1 : string, a2 : string) : jQueryResult {
-        var t = this._v as __noconvert__ jQueryNative;
-        t.css(a1, a2);
-        return this;
+    function css(a1 : string, a2 : string) : jQuery {
+        var t = this._t as __noconvert__ jQueryNative;
+        return t.css(a1, a2);
+    }
+
+    function get(i : number) : Element {
+        var t = this._t as __noconvert__ Map.<Element>;
+        return t[i as string];
     }
 }
 
 native __fake__ class jQueryNative {
-    function css(a1:string, a2:string) : void;
+    function css(a1 : string, a2 : string) : jQuery;
 }
--- use_native_class/main.jsx
+++ use_native_class/main.jsx
@@ -1,10 +1,7 @@
 import "jquery-1.7.jsx";
 class _Main {
     static function main(args : string[]) : void {
-        var jQ = jQuery.as_func;
-        var value : string = jQ("p").get(0).innerHTML;
-        log value + "---" + value;
-        jQ("p").css("background", "red");
-        jQ("p").get(1).innerHTML = "piyo";
+        new jQuery("p").css("background", "red");
+        log new jQuery("p").get(0).innerHTML;
     }
 }
